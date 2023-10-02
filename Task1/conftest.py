754089cb4f05ebddf914ad9cc7d07784adf19037
Liubov <lyubasik85@rambler.ru>
2023-10-02 19:54:21 +0300

    HomeWork

--- Task1/conftest.py
+++ Task1/conftest.py
@@ -6,20 +6,17 @@ import requests
 with open('config.yaml') as f:
     conf = yaml.safe_load(f)
 
-url = conf['url']
-url1 = conf['url1']
-
 
 @pytest.fixture()
 def login():
-    obj_data = requests.post(url=url, data={'username': 'ElenaIvan', 'password': 'b0f5c4418d'})
+    obj_data = requests.post(url=conf['url'], data={'username': 'ElenaIvan', 'password': 'b0f5c4418d'})
     token = obj_data.json()['token']
     return token
 
 
 @pytest.fixture()
 def post():
-    obj_data = requests.post(url=url1, headers={"X-Auth-Token": conf['token']},data={
+    obj_data = requests.post(url=conf['url1'], headers={"X-Auth-Token": conf['token']},data={
         'username': 'ElenaIvan',
         'password': 'b0f5c4418d',
         'title': 'newTitle',
